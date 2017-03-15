3ef8afb0a0c837fb7f8c51f253a67d80738688f4
GarenLiang <ideabreeze@gmail.com>
2017-03-14 21:20:20 -0700

    Action Test

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
   constructor(props) {
     super(props);
     this.state = { comment: ' ' };
   }
 
   HandleChange(event) {
+
       this.setState({ comment: event.target.value });
   }
 
   HandleSubmit(event) {
+    event.preventDefault();
+    this.props.saveComment(this.state.comment);
     this.setState({ comment : '' });
   }
 
@@ -25,3 +30,5 @@ export default class CommentBox extends Component {
     );
   }
 }
+
+export default connect(null, actions)(CommentBox);
--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -5,14 +5,14 @@ describe('CommentList', () => {
   let component;
 
   beforeEach(() => {
-    const props = { comments: ['New Comment', 'Other Comment'] };
+    const props = { comments: ['New Comment', 'Other New Comment'] };
     component = renderComponent(CommentList, null, props);
   });
   it('shows an LI for each comment', () => {
-    expect(component.find('li'.length).to.equal(2));
+    expect(component.find('li').length).to.equal(2);
   });
   it('shows each comment that is provided', () => {
     expect(component).to.contain('New Comment');
-    expect(component).to.contain('Other Comment');
+    expect(component).to.contain('Other New Comment');
   });
 });
